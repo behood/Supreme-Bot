3da471d26dd0bd034ba88a7c42eefe83fb18362e
Brendan <bhood4064@gmail.com>
2018-12-19 12:54:43 -0500

    Project Setup Changes

--- src/SupremeBot.java
+++ src/SupremeBot.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-public class SupremeBotV1 {
+public class SupremeBot {
 
 	private static ArrayList<String> dataList = new ArrayList<String>();
 
